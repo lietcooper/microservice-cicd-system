093b15b203753af1d33d1d64be2e2c9df6c5724a
坚持不懈的格格2代 <yange@jianchibuxiedegege2daideMacBook-Pro.local>
2026-02-22 17:47:31 -0800

    Remove duplicate @EnableJpaRepositories from Application.java

--- server/src/main/java/cicd/Application.java
+++ server/src/main/java/cicd/Application.java
@@ -2,10 +2,8 @@ package cicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "cicd.persistence.repository")
 public class Application {
 
     public static void main(String[] args) {
